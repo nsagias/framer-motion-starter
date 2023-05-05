c814d47121f84979cd104a7fb133e47939ed94e3
Nick <nsagias@gmail.com>
2023-05-05 19:34:12 -0400

    Add moving div horizontally

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,27 @@ import './App.css';
 import { motion } from "framer-motion";
 
 function App() {
-  const [rotateDiv, setRotateDiv] = useState(false);
+  const [rotateDiv, setRotateDiv] = useState<boolean>(false);
+  const [moveDiv, setMoveDiv] = useState<boolean>(false);
   return (
     <div className="framer-container">
-      <motion.div animate={{ y: -100, scale: 1 }} initial={{ scale: 0}}></motion.div>
+      {/* Position Square Div On Load */}
       <motion.div 
-        animate={{ y: 100, scale: 1, rotate: rotateDiv ? 360 : 0 }} 
+        animate={{ y: -100, scale: 1 }} 
         initial={{ scale: 0}}
+      ></motion.div>
+
+      {/* Move Div Horizontally On Click*/}
+      <motion.div 
+        animate={{ y: -50, scale: 1, x: moveDiv ? 200 : -200 }} 
+        initial={{ scale: 0}}
+        onClick={() => setMoveDiv(!moveDiv)}
+      ></motion.div>
+
+      {/*  Rotate Div On Click */}
+      <motion.div 
+        animate={{ y: 100, scale: 1, rotate: rotateDiv ? 45 : 0 }} 
+        initial={{ scale: 0, rotate: 45 }}
         onClick={() => setRotateDiv(!rotateDiv)}
       ></motion.div>
     </div>
