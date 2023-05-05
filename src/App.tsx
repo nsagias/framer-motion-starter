c5a4ba8f108d018b8ec7ef2ce877767c329fbf27
Nick <nsagias@gmail.com>
2023-05-05 19:18:59 -0400

    create two square divs anad animate

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { motion } from "framer-motion";
 
 function App() {
+  const [rotateDiv, setRotateDiv] = useState(false);
   return (
     <div className="framer-container">
+      <motion.div animate={{ y: -100, scale: 1 }} initial={{ scale: 0}}></motion.div>
+      <motion.div animate={{ y: 100, scale: 1 }} initial={{ scale: 0}}></motion.div>
     </div>
   );
 }
