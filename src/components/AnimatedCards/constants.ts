ef783892ee81c578fbd01a2b249f772096b2de0b
Nick <nsagias@gmail.com>
2023-05-06 18:23:34 -0400

    create constants food and carVariants

---
+++ src/components/AnimatedCards/constants.ts
@@ -0,0 +1,27 @@
+import { motion, Variants } from "framer-motion";
+
+export const cardVariants: Variants = {
+  offscreen: {
+    y: 300
+  },
+  onscreen: {
+    y: 50,
+    rotate: -10,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8
+    }
+  }
+};
+
+export const food: [string, number, number][] = [
+  ["🍅", 340, 10],
+  ["🍊", 20, 40],
+  ["🍋", 60, 90],
+  ["🍐", 80, 120],
+  ["🍏", 100, 140],
+  ["🫐", 205, 245],
+  ["🍆", 260, 290],
+  ["🍇", 290, 320]
+];
