79c746b605b33f7ff038609ef2b95d9f9e2d2d94
Nick <nsagias@gmail.com>
2023-05-06 19:37:46 -0400

    update as component

--- src/components/AnimatedCards/AnimatedCard.tsx
+++ src/components/AnimatedCards/AnimatedCard.tsx
@@ -11,16 +11,18 @@ export default function AnimatedCard({ emoji, hueA, hueB }: AnimatedCardProps) {
   const background = `linear-gradient(306deg, ${hue(hueA)}, ${hue(hueB)})`;
 
   return (
-    <motion.div
-      className="card-container"
-      initial="offscreen"
-      whileInView="onscreen"
-      viewport={{ once: true, amount: 0.8 }}
-    >
-      <div className="splash" style={{ background }} />
-      <motion.div className="card" variants={cardVariants}>
-        {emoji}
+    <div id="animated-card-example-1">
+      <motion.div
+        className="card-container"
+        initial="offscreen"
+        whileInView="onscreen"
+        viewport={{ once: true, amount: 0.8 }}
+      >
+        <div className="splash" style={{ background }} />
+        <motion.div className="card" variants={cardVariants}>
+          {emoji}
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </div>
   );
 }
