37d0850f3e74467bc2bf66b5ec957bf688163a8f
Nick <nsagias@gmail.com>
2023-05-06 19:17:42 -0400

    add css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
-import './App.css';
-import { motion } from "framer-motion";
 import AnimatedDiv from './components/AnimatedDiv/AnimatedDiv';
+import AnimatedCard from './components/AnimatedCards/AnimatedCard';
+import { fruitList } from './components/AnimatedCards/constants';
+
+
 
 function App() {
   return (
     <div className="">
-
+      { fruitList && fruitList.map((fruit: any) => (
+        <AnimatedCard emoji={fruit.emoji} hueA={fruit.hueA} hueB={fruit.hueB} key={fruit.emoji} />
+      ))}
+  
     </div>   
   );
 }
--- src/components/AnimatedCards/AnimatedCard.tsx
+++ src/components/AnimatedCards/AnimatedCard.tsx
@@ -1,7 +1,26 @@
 import { motion } from "framer-motion";
+import { AnimatedCardProps } from "../../types/AnimatedCard";
+import { cardVariants } from "./constants";
+import "./AnimatedCard.css"
 
-export default function AnimatedCard() {
+
+
+export default function AnimatedCard({ emoji, hueA, hueB }: AnimatedCardProps) {
+
+  const hue = (h: number) => `hsl(${h}, 100%, 50%)`;
+  const background = `linear-gradient(306deg, ${hue(hueA)}, ${hue(hueB)})`;
+
   return (
-    <div className=""></div>
+    <motion.div
+      className="card-container"
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+    >
+      <div className="splash" style={{ background }} />
+      <motion.div className="card" variants={cardVariants}>
+        {emoji}
+      </motion.div>
+    </motion.div>
   );
 }
