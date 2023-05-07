31e8302a44a5b2440b360b65270910cfb440e808
Nick <nsagias@gmail.com>
2023-05-07 18:36:26 -0400

    created animated buttons

--- src/components/AnimatedButton/AnimatedButton.tsx
+++ src/components/AnimatedButton/AnimatedButton.tsx
@@ -1,5 +1,39 @@
+import { motion } from "framer-motion";
+
 export default function AnimatedButton() {
   return (
-    <h1>AnimatedButton</h1>
+    <>
+      <div>
+        <motion.button 
+          className="save-button"
+          whileHover={{ scale: 1.1 }}
+          onClick={() => null}
+        >
+          Animated Button Hover
+        </motion.button>
+      </div>
+
+      <div>
+        <motion.button 
+          className="save-button"
+          whileTap={{ scale: 0.9 }}
+          onClick={() => null}
+        >
+          Animated Button Tap
+        </motion.button>
+      </div>
+
+      <div>
+        <motion.button 
+          className="save-button"
+          whileTap={{ scale: 0.9 }}
+          whileHover={{ scale: 1.1 }}
+          onClick={() => null}
+        >
+          Animated Button Hover and Tap
+        </motion.button>
+      </div>
+    </>
+  
   );
 }
