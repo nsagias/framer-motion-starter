5e14e99aa62b59ea34228c94cdab57a75dde39ca
Nick <nsagias@gmail.com>
2023-05-07 20:52:12 -0400

    add state and create modal functions, pass down as props to button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import AnimatedButton from './components/AnimatedButton/AnimatedButton';
 import AnimatedModel from './components/AnimatedModal/AnimatedModal';
 
 export default function App() {
+  const [modelOpen, setModalOpen] = useState<boolean>(false);
+
+  const close: () => void = () => setModalOpen(false);
+  const open: () => void = () => setModalOpen(true);
   return (
     <>
+      <AnimatedButton 
+        onModalOpen={modelOpen}
+        onOpen={open}
+        onClose={close}
+      />
       <AnimatedModel />
     </>
   );
